391ed95d63e79807a47e5ea8a3016cf277d8ce17
Melipatamaa <pyrollineisafirefox@gmail.com>
2023-01-31 15:36:51 +0100

    fin du tuto !

--- main.py
+++ main.py
@@ -35,15 +35,20 @@ def draw_grid(win,grid):
         for j in range(PX_H + 1):
             pygame.draw.line(win,LGREY,(j*PX_SIZE,0),(j*PX_SIZE,HEIGHT-TOOLBAR_H))
 
-def draw(win, grid):
+def draw(win, grid, buttons):
     win.fill(BACKGROUND_COLOR)
     draw_grid(win,grid)
+    for button in buttons:
+        button.draw(win)
     pygame.display.update()
 
 def get_coord_position(pos):
     x, y = pos
-    rox = x // PX_SIZE
-    col = y // PX_SIZE
+    row = y // PX_SIZE
+    col = x // PX_SIZE
+    if row >= PX_H:
+        raise IndexError
+    return row, col
     
 
 ## ► Program ◄
@@ -54,6 +59,12 @@ clock = pygame.time.Clock()
 grid = initial_grid(PX_H,PX_W,BACKGROUND_COLOR)
 drawing_col = BLACK
 
+button_y = HEIGHT - TOOLBAR_H/2 - 25
+buttons = [
+    Button(10, button_y, 50, 50, BLACK),
+    Button(70, button_y, 50, 50, ORANGE),
+    Button(130, button_y, 50, 50, WHITE, "erase", LGREY)
+    ]
 
 while using: #run while the user does not close the window
 
@@ -64,9 +75,17 @@ while using: #run while the user does not close the window
         if event.type == pygame.QUIT:
             using = False
 
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0]: #0 for left mouse button
             position = pygame.mouse.get_pos()
+            try :
+                row, col = get_coord_position(position)
+                grid[row][col] = drawing_col
+            except IndexError:
+                for button in buttons:
+                    if not button.clicked(position):
+                        continue
+                    drawing_col = button.color
 
-    draw(WINDOW, grid)
+    draw(WINDOW, grid, buttons)
 
 pygame.quit()
